2c4542201cf44ff0637c4ed6ae2c36b121564ac9
Washi <washiwashi1337@gmail.com>
2018-06-17 01:59:00 +0200

    Standard search algos.

---
+++ Rivers.Test/Analysis/SearchTest.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using Rivers.Analysis;
+using Xunit;
+
+namespace Rivers.Test.Analysis
+{
+    public class SearchTest
+    {
+        private static readonly Graph Tree;
+
+        static SearchTest()
+        {
+            Tree = new Graph();
+            Tree.Nodes.Add("1");
+            Tree.Nodes.Add("2");
+            Tree.Nodes.Add("3A");
+            Tree.Nodes.Add("4A");
+            Tree.Nodes.Add("5");
+            Tree.Nodes.Add("6");
+
+            Tree.Edges.Add("1", "2");
+            Tree.Edges.Add("1", "3A");
+            Tree.Edges.Add("2", "4A");
+            Tree.Edges.Add("2", "5");
+            Tree.Edges.Add("3A", "6");
+        }
+
+        [Fact]
+        public void BreadthFirstTest()
+        {
+            Assert.Contains(Tree.Nodes["1"].BreadthFirstSearch(n => n.OutgoingEdges.Count == 0), new []
+            {
+                Tree.Nodes["4A"],
+                Tree.Nodes["5"],
+                Tree.Nodes["6"],
+            });
+        }
+
+        [Fact]
+        public void DepthFirstTest()
+        {
+            Assert.Contains(Tree.Nodes["1"].DepthFirstSearch(n => n.OutgoingEdges.Count == 0), new []
+            {
+                Tree.Nodes["4A"],
+                Tree.Nodes["5"],
+                Tree.Nodes["6"],
+            });
+        }
+
+        [Fact]
+        public void BreadthFirstOrderTest()
+        {
+            var order = Tree.Nodes["1"].BreadthFirstTraversal().ToList();
+            Assert.True(order.IndexOf(Tree.Nodes["2"]) < order.IndexOf(Tree.Nodes["4A"]));
+            Assert.True(order.IndexOf(Tree.Nodes["3A"]) < order.IndexOf(Tree.Nodes["4A"]));
+        }
+        
+        [Fact]
+        public void DepthFirstOrderTest()
+        {
+            var order = Tree.Nodes["1"].DepthFirstTraversal().ToList();
+            if (order.IndexOf(Tree.Nodes["2"]) < order.IndexOf(Tree.Nodes["3A"]))
+                Assert.True(order.IndexOf(Tree.Nodes["4A"]) < order.IndexOf(Tree.Nodes["3A"]));
+            else
+                Assert.True(order.IndexOf(Tree.Nodes["5"]) < order.IndexOf(Tree.Nodes["4A"]));
+        }
+    }
+}
---
+++ Rivers/Analysis/Search.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rivers.Analysis
+{
+    /// <summary>
+    /// Provides a collection of standard graph search algorithms.
+    /// </summary>
+    public static class Search
+    {
+        /// <summary>
+        /// Performs a search for a node in a graph in a breadth first order.
+        /// </summary>
+        /// <param name="start">The node in the graph to start the search at.</param>
+        /// <param name="predicate">The predicate the to-be-searched node needs to satisfy.</param>
+        /// <returns>The found node, or <c>null</c> if none was found.</returns>
+        public static Node BreadthFirstSearch(this Node start, Predicate<Node> predicate)
+        {
+            return BreadthFirstTraversal(start).FirstOrDefault(new Func<Node, bool>(predicate));
+        }
+
+        /// <summary>
+        /// Performs a traversal all descendants of a node in a breadth first order, and yields
+        /// on every node it visits.
+        /// </summary>
+        /// <param name="start">The node in the graph to start the traversal at.</param>
+        /// <returns>A lazy loaded ordered collection representing all nodes it traversed.</returns>
+        public static IEnumerable<Node> BreadthFirstTraversal(this Node start)
+        {
+            var queue = new Queue<Node>();
+            var visited = new HashSet<Node>();
+
+            queue.Enqueue(start);
+            
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                
+                if (visited.Add(current))
+                {
+                    yield return current;
+                    foreach (var edge in current.OutgoingEdges)
+                        queue.Enqueue(edge.Target);
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Performs a search for a node in a graph in a depth first order.
+        /// </summary>
+        /// <param name="start">The node in the graph to start the search at.</param>
+        /// <param name="predicate">The predicate the to-be-searched node needs to satisfy.</param>
+        /// <returns>The found node, or <c>null</c> if none was found.</returns>
+        public static Node DepthFirstSearch(this Node start, Predicate<Node> predicate)
+        {
+            return DepthFirstTraversal(start).FirstOrDefault(new Func<Node, bool>(predicate));
+        }
+
+        /// <summary>
+        /// Performs a traversal all descendants of a node in a depth first order, and yields
+        /// on every node it visits.
+        /// </summary>
+        /// <param name="start">The node in the graph to start the traversal at.</param>
+        /// <returns>A lazy loaded ordered collection containing all nodes it traversed.</returns>
+        public static IEnumerable<Node> DepthFirstTraversal(this Node start)
+        {            
+            var stack = new Stack<Node>();
+            var visited = new HashSet<Node>();
+
+            stack.Push(start);
+            
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                if (visited.Add(current))
+                {
+                    yield return current;
+                    foreach (var edge in current.OutgoingEdges)
+                        stack.Push(edge.Target);
+                }
+            }
+        }
+        
+        
+    }
+}
